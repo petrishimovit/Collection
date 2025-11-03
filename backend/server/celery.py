a60b0064c4dce517122960882ee0d700903d128b
Petr <petr.ishimov@outlook.com>
2025-11-03 19:18:49 +0000

    feat(games): add docker-compose for redis

--- backend/server/celery.py
+++ backend/server/celery.py
@@ -1,7 +1,22 @@
 import os
 from celery import Celery
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+
 app = Celery("server")
-app.config_from_object("django.conf:settings", namespace="CELERY")
+
+
+app.conf.broker_url = "redis://localhost:6379/0"
+app.conf.result_backend = "redis://localhost:6379/0"
+
+
 app.autodiscover_tasks()
+
+
+app.conf.update(
+    timezone="UTC",
+    enable_utc=True,
+    task_track_started=True,
+    task_time_limit=30 * 60,
+)
