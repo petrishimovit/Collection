adb73e0ef6a5a58d4fec0bcb58b48e4d7b41c77a
Petr <petr.ishimov@outlook.com>
2025-11-29 14:16:02 +0000

    feat(collections): add is_active field into all models (for soft delete)

--- backend/core/models.py
+++ backend/core/models.py
@@ -2,13 +2,34 @@ import uuid
 from django.db import models
 
 
+class ActiveManager(models.Manager):
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(is_active=True)
+
+
 class BaseModel(models.Model):
     """
     Abstract Basemodel
     """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+
+    is_active = models.BooleanField(
+        default=True,
+        db_index=True,
+    )
+
+   
+    objects = ActiveManager()
+    all_objects = models.Manager()
 
     class Meta:
         abstract = True
@@ -16,3 +37,5 @@ class BaseModel(models.Model):
 
     def __str__(self):
         return f"{self.__class__.__name__}({self.pk})"
+
+ 
