4198f698361681d91c0f50ee626f8727dfd6a1bb
Petr <petr.ishimov@outlook.com>
2025-12-12 12:40:50 +0000

    feat(notifications): add notifications model

--- backend/apps/notifications/models.py
+++ backend/apps/notifications/models.py
@@ -1,3 +1,43 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel  
+
+
+class Notification(BaseModel):
+    
+    """
+    Notification model for User`s
+    """
+
+    class Type(models.TextChoices):
+        FOLLOW = "follow"
+        POST_CREATE = "post"
+        COMMENT_CREATE = "comment_create"
+        ITEM_CREATE = "item_create"
+        
+
+    for_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="notifications",
+        db_index=True,
+    )
+
+    type = models.CharField(
+        max_length=32,
+        choices=Type.choices,
+        db_index=True,
+    )
+
+
+    is_checked = models.BooleanField(
+        default=False,
+        db_index=True,
+    )
+
+    class Meta(BaseModel.Meta):
+        indexes = [
+            models.Index(fields=["for_user", "is_checked", "-created_at"]),
+            models.Index(fields=["for_user", "-created_at"]),
+        ]
